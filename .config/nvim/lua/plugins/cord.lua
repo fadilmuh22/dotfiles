f572aad746f36abbaac5580abf05820067fea5e9
fadilmuh22 <fadilmuh2002@gmail.com>
2024-12-20 18:06:18 +0700

    feat: cord

--- .config/nvim/lua/plugins/cord.lua
+++ .config/nvim/lua/plugins/cord.lua
@@ -2,5 +2,19 @@ return {
   'vyfor/cord.nvim',
   build = './build || .\\build',
   event = 'VeryLazy',
-  opts = {}, -- calls require('cord').setup()
+  opts = {
+    buttons = {
+      {
+        label = 'View Repository', -- Text displayed on the button
+        url = 'git',             -- URL where the button leads to ('git' = automatically fetch Git repository URL)
+      },
+      {
+        label = 'View Plugin',
+        url = 'https://github.com/vyfor/cord.nvim',
+      }
+    },
+  },
+  config = function(_, opts)
+    require('cord').setup { opts }
+  end
 }
